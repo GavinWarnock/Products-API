ef62e2315d280592393394d92b234552b5a862a2
Gavin Warnock <gbwarnock@gmail.com>
2023-03-10 09:27:29 -0700

    feat: created game class in the models section

--- app.py
+++ app.py
@@ -23,7 +23,15 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.String(255), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    inventory_quantity = db.Column(db.Integer)
 
+    def __repr__(self):
+        return f'{self.name} {self.description} {self.price} {self.inventory_quantity}'
 
 
 # Schemas
